9da981d824265c13fbfd3e16462817359a14125f
Morris Alper <morrisalp@gmail.com>
2025-08-14 23:51:13 -0400

    expanded details

--- src/dictionary-data.js
+++ src/dictionary-data.js
@@ -4,41 +4,99 @@ const placeholderEntries = [
     {
         word: "íijang",
         definition: "to be",
-        type: "verb"
+        type: "verb",
+        grammar: "intransitive verb (-s)",
+        examples: [
+            {
+                sentence: "Hláa uu íijang.",
+                translation: "It's me."
+            },
+            {
+                sentence: "Hal íijang uu?",
+                translation: "Where are you?"
+            }
+        ],
+        inflections: ["is", "íijang", "íijan", "ijáan"],
+        notes: "This is the basic copula verb in Haida, equivalent to 'to be' in English. Cf. g̱íidang."
     },
     {
         word: "g̱ándl",
         definition: "water",
-        type: "noun"
+        type: "noun",
+        grammar: "common noun",
+        examples: [
+            {
+                sentence: "G̱ándlaay k'íinaang.",
+                translation: "The water is hot.."
+            }
+        ],
+        inflections: ["g̱ándlaay (the water)"],
+        notes: ""
     },
     {
         word: "hlg̱ánggula",
         definition: "to work",
-        type: "verb"
+        type: "verb",
+        grammar: "intransitive verb (weak-a)",
+        examples: [
+            {
+                sentence: "Dáng hlg̱ánggulaang.",
+                translation: "You work."
+            }
+        ],
+        inflections: ["hlg̱ánggulgan", "hlg̱ánggulsaang"],
+        notes: ""
     },
     {
         word: "na",
         definition: "house",
-        type: "noun"
+        type: "noun",
+        grammar: "common noun (classifier: tíi)",
+        examples: [
+            {
+                sentence: "Náay uu í'waangang.",
+                translation: "The house is big."
+            }
+        ],
+        inflections: ["náay (the house)"],
+        notes: ""
     },
     {
-        word: "táaw",
-        definition: "fish",
-        type: "noun"
+        word: "chíin",
+        definition: "fish, salmon",
+        type: "noun",
+        grammar: "common noun",
+        examples: [
+            {
+                sentence: "Chíin uu táaw 'láa íijang.",
+                translation: "Fish is a good food.."
+            }
+        ],
+        inflections: ["chíinaay (the fish)"],
+        notes: "Refers either to salmon or fish in general."
     },
     {
-        word: "x̱aad",
-        definition: "person, Haida",
-        type: "noun"
+        word: "X̱aad Kíl",
+        definition: "Haida language",
+        type: "noun",
+        grammar: "compund noun",
+        examples: [
+            {
+                sentence: "Dámaan X̱aad Kíl hal gúusuugang.",
+                translation: "S/he speaks Haida fluently."
+            }
+        ],
+        inflections: [],
+        notes: "Related words: X̱aad, Kíl."
     },
     {
-        word: "ĝáaw",
-        definition: "crow",
-        type: "noun"
-    },
-    {
-        word: "k'ayáang",
-        definition: "to sleep",
-        type: "verb"
+        word: "naĝats'íi",
+        definition: "fox",
+        type: "noun",
+        grammar: "common noun",
+        examples: [
+        ],
+        inflections: [],
+        notes: ""
     }
 ];
